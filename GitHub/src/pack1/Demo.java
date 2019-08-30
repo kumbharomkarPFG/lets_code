22888e6a228e3f80e8ed829fd6d3dcc25eaf174a
K687100 <K687100@VW10A15734.principalusa.corp.principal.com>
2019-08-30 15:39:56 +0530

    something related to interface has been created.

--- GitHub/src/pack1/Demo.java
+++ GitHub/src/pack1/Demo.java
@@ -1,10 +1,11 @@
 package pack1;
-
+import pack1.MyInterface;
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyInterface ref=System.out::println;
+		ref.disp("hello there");
 	}
 
 }
